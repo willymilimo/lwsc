9eb1245bd47247398bcc211519293a054e72bb11
Milli <milicodemoonga@gmail.com>
2020-07-09 01:22:21 +0200

    revised notification interface

--- models/notification.ts
+++ models/notification.ts
@@ -1,28 +1,30 @@
-import { NotificationType, NotificationStatus } from "../types/notification";
-
 export interface NotificationI {
-  _id?: string;
+  // _id?: string;
+  // title: string;
+  // type: NotificationType;
+  // date: Date | string;
+  // message: string;
+  // status?: NotificationStatus;
+
+  _id: string;
   title: string;
-  type: NotificationType;
-  date: Date | string;
-  message: string;
-  status?: NotificationStatus;
+  description: string;
+  icon: string;
+  create_on: Date;
 }
 
 export class Notification implements NotificationI {
-  _id?: string;
+  _id: string;
   title: string;
-  type: NotificationType;
-  date: string | Date;
-  message: string;
-  status?: NotificationStatus;
+  description: string;
+  icon: string;
+  create_on: Date;
 
-  constructor({ _id, title, type, date, message, status }: NotificationI) {
+  constructor({ _id, title, description, icon, create_on }: NotificationI) {
     this._id = _id;
     this.title = title;
-    this.date = date;
-    this.type = type as NotificationType; // potentially buggy use of keyword
-    this.message = message;
-    this.status = status as NotificationStatus;
+    this.description = description;
+    this.icon = icon;
+    this.create_on = new Date(create_on);
   }
 }
